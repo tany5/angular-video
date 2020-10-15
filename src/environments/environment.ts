ba8ea96ef9173e54d802998321ddde8d568f1f8c
unknown <anirban.visiable@gmail.com>
2020-10-15 19:14:23 +0530

    updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  agora: {
+    appId: '191aa009f59c40c2b96f9a42e4f94436'
+  }
 };
 
 /*
